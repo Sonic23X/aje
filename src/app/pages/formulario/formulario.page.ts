844e8c722f279022942fcce14b3fd81ba307525b
Alfred Ortega <omar.alfredo49@gmail.com>
2020-09-28 20:11:26 -0500

    Finish formulario

--- src/app/pages/formulario/formulario.page.ts
+++ src/app/pages/formulario/formulario.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-formulario',
@@ -7,22 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioPage implements OnInit {
 
-  pregunta1: string;
-  pregunta2: string;
-  pregunta3: string;
-  pregunta4: string;
-  pregunta5: string;
-  pregunta6: string;
-  pregunta7: string;
-  pregunta8: string;
-  pregunta9: string;
-  pregunta10: string;
-  pregunta11: string;
-  pregunta12: string;
-  pregunta13: string;
-  pregunta14: string;
+  pregunta1: string = '';
+  pregunta2: string = '';
+  pregunta3: string = '';
+  pregunta4: string = '';
+  pregunta5: string = '';
+  pregunta6: string = '';
+  pregunta7: string = '';
+  pregunta8: string = '';
+  pregunta9: string = '';
+  pregunta10: string = '';
+  pregunta11: string = '';
+  pregunta12: string = '';
+  pregunta13: string = '';
+  pregunta14: string = '';
 
-  constructor() { }
+  constructor( private navCtrl: NavController ) { }
 
   ngOnInit() {
   }
@@ -78,6 +80,21 @@ export class FormularioPage implements OnInit {
 
   evaluar( )
   {
+
+    if ( this.pregunta1 == '' || this.pregunta2 == '' || this.pregunta3 == '' || this.pregunta4 == '' || this.pregunta5 == '' ||
+         this.pregunta6 == '' || this.pregunta7 == '' || this.pregunta8 == '' || this.pregunta9 == '' || this.pregunta10 == '' ||
+         this.pregunta11 == '' || this.pregunta12 == '' || this.pregunta13 == '' || this.pregunta14 == '')
+    {
+      Swal.fire(
+      {
+        title: 'Error',
+        text: 'Todos los campos son obligatorios',
+        icon: 'error',
+      });
+
+      return;
+    }
+
     let a = 0;
     let b = 0;
     let c = 0;
@@ -308,17 +325,30 @@ export class FormularioPage implements OnInit {
     }
 
     if ( a > b && a > c && a > d )
-      console.log( 'El mayor es a' );
+      Swal.fire(
+      {
+        title: 'Tu nivel es CONOCIDO',
+        text: 'Has hecho un contacto inicial con la Iglesia, y te caracteriza el interés, aunque aún no desarrollas un compromiso más grande. <br>Te mueves principalmente en el sentido de la vista y para ti las impresiones son importantes. Estás en la etapa de desarrollar empatía. <br>Te recomendamos que te bautices pronto si aún no lo haces y que busques rápidamente a alguien que te acompañe en tu crecimiento y pronto te bautices.'
+      });
     
     if ( b > a && b > c && b > d )
-      console.log( 'El mayor es b' );
+      Swal.fire({
+        title: 'Tu nivel es SIERVO',
+        text: 'Ya has puesto tu fe en Cristo, te bautizaste y te uniste a una Iglesia local. Estás echando raíces y quizá sirves como voluntario en algún ministerio.<br> Estás desarrollando la mentalidad correcta y subiendo tu nivel de compromiso.<br> Te mueves principalmente por lo que escuchas y por lo que tu meta es seguir instrucciones para tu crecimiento.<br>Te recomendamos que sigas de cerca a un hermano mayor, y resistas la presión de críticas, burlas y problemas que te puedan hacer frente. Debes afirmar tu comunión con Dios de manera disciplinada con la lectura de la Biblia y la oración. Desarrolla obediencia',
+      });
 
     if ( c > a && c > b && c > d )
-      console.log( 'El mayor es c' );
+      Swal.fire({
+        title: 'Tu nivel es AMIGO',
+        text: 'Te has vuelto confiable en la Iglesia a la que perteneces.<br> Has desarrollado por tiempo una actitud de servicio fiel y has crecido en madurez, aunque aún estás en el proceso de dar el fruto completo y correcto en el Reino.<br>Te recomendamos que aprendas el valor de amar y honrar a tus pastores y líderes y abraces la revelación de tu iglesia.<br> Tu palabra clave es intimidad para desarrollar fidelidad.',
+      });
 
     if ( d > a && d > b && d > c )
-      console.log( 'El mayor es d' );
-      
-
+      Swal.fire({
+        title: 'Tu nivel es HIJO',
+        text: 'Eres un discípulo que ha desarrollado completa su identidad.<br> Estás aprendiendo a operar como rey y sacerdote, fuera de las cuatro paredes de tu iglesia y te preparas para guiar el avance del Reino con los énfasis propios de la casa que te formó.<br> Te has vuelto un hacedor, así que la meta es que ocupes la posición a la que Dios te llamó.<br>Te recomendamos mantener tu lealtad a tu iglesia, prepararte en cómo expresar tu ministerio en el ámbito social, fuera del templo, y te mantengas en la llenura del espíritu para funcionar bien en tu asignación',
+      });
+    
+      this.navCtrl.navigateRoot('/home');
   }
 }
